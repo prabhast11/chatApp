497a56952936e70b19187cf8bf25081781ff51f2
prabhast11 <prabhast11@gmail.com>
2023-02-24 13:44:55 +0530

    added context api createcontext code

---
+++ frontend/src/Context/chatProvider.js
@@ -0,0 +1,13 @@
+import { createContext } from 'react'
+
+const ChatContext = createContext()
+
+const ChatProvider = ({ children }) => {
+    return(
+        <ChatContext.Provider>
+            {children}
+        </ChatContext.Provider>
+    )
+}
+
+export default ChatProvider
